1a38bb0ac522861a68e7dd0236cd5583047e8752
Minah Kim <kimma1205@gmail.com>
2025-08-23 01:50:16 +0900

    CSS 클래스와 함께 HTML 태그 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,171 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import ArtistPediaPage from "./pages/artist-name/artistpedia/ArtistPediaPage";
-import ArtistInfoModal from "./pages/artist-name/artistpedia/ArtistInfoModal";
-import FeedPage from "./pages/artist-name/feed/FeedPage";
-import FanPostModal from "./pages/artist-name/feed/FanPostModal";
-import ArtistPage from "./pages/artist-name/artist/ArtistPage";
-import ArtistPostModal from "./pages/artist-name/artist/ArtistPostModal";
-import MomentPostPage from "./pages/artist-name/moment/MomentPostPage";
-import ArtistProfilePage from "./pages/artist-name/profile/ArtistProfilePage";
-import ArtistProfileMemberPage from "./pages/artist-name/profile/ArtistProfileMemberPage";
-import DigitalMembershipModal from "./pages/artist-name/digitalmembership/DigitalMembershipModal";
-import MediaAllPage from "./pages/artist-name/media/MediaAllPage";
-import MediaContPage from "./pages/artist-name/media/MediaContPage";
-import MediaNewPage from "./pages/artist-name/media/MediaNewPage";
-import MediaRecommendPage from "./pages/artist-name/media/MediaRecommendPage";
-import MediaMembershipPage from "./pages/artist-name/media/MediaMembershipPage";
-import MediaDetailPage from "./pages/artist-name/media/MediaDetailPage";
-import LiveIndexPage from "./pages/artist-name/live/LiveIndexPage";
-import LiveDetailPage from "./pages/artist-name/live/LiveDetailPage";
-import NoticePage from "./pages/artist-name/notice/NoticePage";
+import './index.css';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* 홈 */}
-        <Route path="/" element={<HomePage />} />
 
-        {/* 아티스트 공통 */}
-        <Route path=":artistName">
-          <Route index element={<Navigate to="feed" replace />} />
+    <div>
+      {/* 헤더 */}
+      <div className="header">
+        <header className="header__container">
+          <div className="header__service">
+            <h1 className="header__logo-wrap">
+              <a className="header__logo-link" href="/" >
+                <span className="blind">weverse</span>
+              </a>
+            </h1>
+          </div>
+          <div className="header__action">
+            <button className="header__sign-in-btn" type="button">Sign in</button>
+            <div className="header__shop-wrap">
+              <button className="header__jelly-shop-btn" type="button">
+                <span className="blind">Jelly Shop</span>
+              </button>
+              <a className="header__weverse-shop-link">
+                <span className="blind">Weverse Shop</span>
+              </a>
+            </div>
+          </div>
+        </header>
+      </div>
 
-          {/* Artistpedia */}
-          <Route path="artistpedia" element={<ArtistPediaPage />} />
-          <Route path="artistpedia/artistinfo" element={<ArtistInfoModal />} />
+      {/* 바디 */}
+      <div className="body">
+        <div className="home__carousel">
+          <div className="home__carousel-area">
+            <div className="home__carousel-inner">
+              <div className="home__carousel-list flicking-viewport">
+                <div className="home__carousel-item flicking-camera">
+                  <div className="home__carousel-item-wrap">
+                    <div className="home__carousel-content" role="button">
+                      <em className="home__carousel-em">KATSEYE</em>
+                      <div className="home__carousel-title-area">
+                        <strong className="home__carousel-title">Available now <br />on Weverse Shop!</strong>
+                        <p className="home__carousel-sub-title">KATSEYE'S VIEWFINDER in Seoul <br /></p>
+                      </div>
+                    </div>
+                    <div className="home__carousel-content" role="button">
+                      <em className="home__carousel-em">2hollis</em>
+                      <div className="home__carousel-title-area">
+                        <strong className="home__carousel-title">2hollis<br />Community Open</strong>
+                        <p className="home__carousel-sub-title">Get closer with him <br />on Weverse!</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="home__carousel-pagination">
+            <span className="home__carousel-page">
+              <span className="blind">1 / 7</span>
+            </span>
+          </div>
+          <button className="home__carousel-prev-btn" type="button">
+            <span className="blind">prev</span>
+          </button>
+          <button className="home__carousel-next-btn" type="button">
+            <span className="blind">next</span>
+          </button>
+        </div>
+        <div className="home__artist">
+          <div className="home__dm">
+            <div className="home__dm-inner">
+              <h2 className="home__dm-title">DM with your Artist!</h2>
+              <div className="home__dm-list">
+                <ul>
+                  <li>
+                    <button className="home__dm-img-btn" type="button">
+                      <span className="home__dm-img-wrap">
+                        <img>
+                        </img>
+                      </span>
+                      <span className="home__dm-artist-name">KATSEYE</span>
+                    </button>
+                  </li>
+                </ul>
+                <ul>
+                  <li>
+                    <button className="home__dm-img-btn" type="button">
+                      <span className="home__dm-img-wrap">
+                        <img>
+                        </img>
+                      </span>
+                      <span className="home__dm-artist-name">KATSEYE</span>
+                    </button>
+                  </li>
+                </ul>
+                <ul>
+                  <li>
+                    <button className="home__dm-img-btn" type="button">
+                      <span className="home__dm-img-wrap">
+                        <img>
+                        </img>
+                      </span>
+                      <span className="home__dm-artist-name">KATSEYE</span>
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="home__artist-slot">
+            <div className="home_-artist-area">
+              <div className="home__artist-inner">
+                <h2 className="home__artist-title">Looking for new artists?</h2>
+                <div className="home__artist-body">
+                  <ul className="home__artist-list">
+                    <li className="home__artist-item">
+                      <a className="home__artist-link" href="/">
+                        <div className="home__artist-cover-wrap"></div>
+                        <div className="home__artist-thumb-wrap"></div>
+                        <div className="home__artist-text-wrap"></div>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="home__footer">
+          <ul className="home__footer-link-list">
+            <li className="home__footer-link-item">
+              <a href="/policies/terms">Terms of use</a>
+            </li>
+            <li className="home__footer-link-item">
+              <a href="/policies/service">Service operation policy</a>
+            </li>
+            <li>
+              <a href="/policies/paid">Premium service terms of use</a>
+            </li>
+            <li>
+              <a href="/policies/youth-protection">Child and Youth Protection Policy</a>
+            </li>
+            <li>
+              <a href="/policies/privacy">Privacy policy</a>
+            </li>
+            <li>
+              <a href="/policies/cookie">Cookie policy</a>
+            </li>
+            <li>
+              <button className="home__footer-btn" type="button">Cookie settings</button>
+            </li>
+            <li>
+              <button className="home__footer-btn" type="button">Join Weverse as an artist</button>
+            </li>
+            <li>
+              <button className="home__footer-btn" type="button">Help</button>
+            </li>
+          </ul>
+          <address className="home__footer-company-info">
+            <p className="home__footer-company-info-text">
+              <span className="home__footer-company-info-text-company">Weverse</span>
+            </p>
+          </address>
+          <div className="home__footer-copyright"></div>
+        </div>
+      </div>
 
-          {/* Fan */}
-          <Route path="feed" element={<FeedPage />} />
-          <Route path="fanpost/:fanPostId" element={<FanPostModal />} />
-
-          {/* Artist */}
-          <Route path="artist" element={<ArtistPage />} />
-          <Route path="artist/:artistPostId" element={<ArtistPostModal />} />
-          
-          {/* 그 외 */}
-          <Route path="moment/:momentId/post/:artistMomentId" element={<MomentPostPage />}/>
-          <Route path="profile" element={<ArtistProfilePage />} />
-          <Route path="profile/:memberId" element={<ArtistProfileMemberPage />}/>
-
-          {/* Digital Membership */}
-          <Route path="digitalmembership" element={<DigitalMembershipModal />}
-          />
-
-          {/* Media */}
-          <Route path="media">
-            <Route index element={<MediaAllPage />} />
-            <Route path="cont" element={<MediaContPage />} />
-            <Route path="new" element={<MediaNewPage />} />
-            <Route path="recommend" element={<MediaRecommendPage />} />
-            <Route path="membership" element={<MediaMembershipPage />} />
-            <Route path="all" element={<MediaAllPage />} />
-            <Route path=":mediaId" element={<MediaDetailPage />} />
-          </Route>
-
-          {/* Live */}
-          <Route path="live">
-            <Route index element={<LiveIndexPage />} />
-            <Route path=":liveId" element={<LiveDetailPage />} />
-          </Route>
-
-          {/* Notice */}
-          <Route path="notice/:noticeId" element={<NoticePage />} />
-        </Route>
-
-        {/* 404 */}
-        <Route path="*" element={<h1>404 Not Found</h1>} />
-      </Routes>
-    </BrowserRouter>
+    </div>
   );
 }
