434bc0ca251eb8baabf767cbe1154e49b5fdc23f
Minah Kim <kimma1205@gmail.com>
2025-09-04 01:21:00 +0900

    ArtistPedia 페이지 html 및 css 작성

--- src/components/ArtistPedia.tsx
+++ src/components/ArtistPedia.tsx
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router-dom';
+import "../index.css";
 import "./ArtistPedia.css";
 
 export default function ArtistPedia() {
@@ -14,37 +15,48 @@ export default function ArtistPedia() {
               <div className="PcArtistPediaJumboIntro_cover__JcGS8" data-cover-type="IMAGE" data-shape-type="HORIZONTAL">
                 <div className="PcArtistPediaJumboImageView_image_wrap__pAIwS" data-shape-type="HORIZONTAL">
                   <div style={{ aspectRatio: 'auto 1500 / 1280' }}>
-                    <img src="https://phinf.wevpstatic.net/MjAyMzA4MTBfMTE1/MDAxNjkxNjQxNzU4MjQ2.I2282NTtCbYSMFrSM-qlVe9e7o0mQZlydcLj-fiqnRYg.LMBmyUmWfdB2wvwlBgtcemk2aNRdUfnbg-uImBNb37Mg.JPEG/image.jpg?type=w2200" className="PcArtistPediaJumboImageView_image__fLqoW" width="1500" height="1280" alt=""></img>
+                    <img src="https://phinf.wevpstatic.net/MjAyMzA4MTBfMTE1/MDAxNjkxNjQxNzU4MjQ2.I2282NTtCbYSMFrSM-qlVe9e7o0mQZlydcLj-fiqnRYg.LMBmyUmWfdB2wvwlBgtcemk2aNRdUfnbg-uImBNb37Mg.JPEG/image.jpg?type=w2200" className="PcArtistPediaJumboImageView_image__fLqoW" width="1100" height="1280" alt="">
+                    </img>
                   </div>
                 </div>
               </div>
               <div className="PcArtistPediaJumboIntro_description__+Aprn">
                 <div>
+                  <img src="https://phinf.wevpstatic.net/MjAyMzA4MTBfMjMz/MDAxNjkxNjQzNTI0MDAz.I6fTzYMnUyl-5BnG9zBPx9BRc-WjQIeDdlrF8RhtdHkg.RO9OKrPnUzUZHYEiJw3IJIprxhLVvAaGZGnLQSesPsIg.PNG/image.png" height="74" alt="">
+                  </img>
                 </div>
               </div>
             </div>
             <div className="ArtistPediaMemberProfiles_pc_profile__f6POE">
               <h3 className="PcArtistPediaTitleView_title__5VRG9">Profile</h3>
               <div className="PcArtistPediaMemberProfiles_flicking_wrap__XJDDY">
-                <div className="flicRng-viewport PcArtistPediaMemberProfiles_flicking__aUn+I" style={{ userSelect: 'none', webkitUserDrag: 'none', touchAction: 'pan-y' }}>
-                  <div className="flicRng-camera" style={{ transform: 'translate(-10px)' }}>
+                <div className="flicking-viewport PcArtistPediaMemberProfiles_flicking__aUn+I" style={{ userSelect: 'none', touchAction: 'pan-y;' }}>
+                  <div className="flicking-camera" style={{ transform: 'translate(-10px)' }}>
                     <a className="PcArtistPediaMemberProfiles_flicking_item__IFEfV" href="/winner/profile/3ae9cc877db32203257f21329499164d">
                       <div style={{ aspectRatio: 'auto 120 / 120', overflow: 'hidden', borderRadius: '30px' }}>
+                        <img src="https://phinf.wevpstatic.net/MjAyMjA3MDZfMjQ0/MDAxNjU3MDg0OTEzNDg2.EzTpaYX-guv9TOjN_GC_SIdnH8n9xcCdV98EVZSEDNsg.V2pB1CiR9vlKk0-HyIaNjaYQDj2G2NMfzV09r-PKY9Ig.PNG/a329c74c46fd4e29aad9e9f3a3f669b7911.png?type=s240" className="ArtistPediaProfileImageView_image__8aUhn" width="120" height="120" alt="">
+                        </img>
                       </div>
-                      <div className="PcArtistPediaMemberProfilesView_artist__AnKzo">YOON</div>
+                      <div className="PcArtistPediaMemberProfilesView_artist__AnKzo">YOON </div>
                     </a>
                     <a className="PcArtistPediaMemberProfiles_flicking_item__IFEfV" href="/winner/profile/57c0567ae4599e186215875b6f4c6136">
                       <div style={{ aspectRatio: 'auto 120 / 120', overflow: 'hidden', borderRadius: '30px' }}>
+                        <img src="https://phinf.wevpstatic.net/MjAyMjA3MDZfMTM1/MDAxNjU3MDc1OTM0Nzgz.pqX9lybqbF7W8Z7R5_SEJXdZKi1WyTbcGX2M37jYMQAg.Tt9dCffXmjqVtLbkBjQpkk2_35SB8_yzDVjv-6gWIH8g.PNG/f4b91a041f9742728bf2593edb7609bf676.png?type=s240" className="ArtistPediaProfileImageView_image__8aUhn" width="120" height="120" alt="">
+                        </img>
                       </div>
                       <div className="PcArtistPediaMemberProfilesView_artist__AnKzo">JINU</div>
                     </a>
                     <a className="PcArtistPediaMemberProfiles_flicking_item__IFEfV" href="/winner/profile/9f20d36dfcd653f5b108a5de45ebeedc">
                       <div style={{ aspectRatio: 'auto 120 / 120', overflow: 'hidden', borderRadius: '30px' }}>
+                        <img src="https://phinf.wevpstatic.net/MjAyMjA3MDZfMjI3/MDAxNjU3MDg0OTI0Mzc3.oEbKT-KinihVz4flu_JAP9OTjKoGAM4aOfH7OpnK7rkg.zbtLvrB3wPTY99lt7MxlLEiy4OKun62fdigPeHJQPnYg.PNG/c79227c3e16d41de95900a03158968a6590.png?type=s240" className="ArtistPediaProfileImageView_image__8aUhn" width="120" height="120" alt="">
+                        </img>
                       </div>
                       <div className="PcArtistPediaMemberProfilesView_artist__AnKzo">MINO</div>
                     </a>
                     <a className="PcArtistPediaMemberProfiles_flicking_item__IFEfV" href="/winner/profile/eae442f350fa798856eba453b8476f24">
                       <div style={{ aspectRatio: 'auto 120 / 120', overflow: 'hidden', borderRadius: '30px' }}>
+                        <img src="https://phinf.wevpstatic.net/MjAyMjA3MDZfMjky/MDAxNjU3MDgyNTMxNDgw.NxUi4ihHeWNHl28qv14YHhfj2pXk8MEyA4PwgsLEzRUg.QOJHzhrCznxFz4588wvYa_18hdL5knWvQaDLkmM5Pckg.PNG/2fb8dfc441904f67866478189860325d207.png?type=s240" className="ArtistPediaProfileImageView_image__8aUhn" width="120" height="120" alt="">
+                        </img>
                       </div>
                       <div className="PcArtistPediaMemberProfilesView_artist__AnKzo">HOONY</div>
                     </a>
@@ -59,6 +71,8 @@ export default function ArtistPedia() {
                   <a className="PcArtistPediaFanPickedLiveItemView_thumbnail_link__exZJV" href="/winner/media/0-155788625">
                     <div className="PcArtistPediaFanPickedLiveItemView_thumbnail_wrap__cKKNC">
                       <div style={{ aspectRatio: 'auto 351 / 220' }}>
+                        <img src="https://phinf.wevpstatic.net/MjAyNTAyMTJfMjg2/MDAxNzM5MzYxMDIwMzg0.nZlJKUGMkjzjk5xIQrs_SnTKAXoi55tBCe0bNS4G5FIg.Vb4lQTZN1T3343_9dCHXAQXZGsPHHcfpq4M_VnFcgjMg.JPEG/thumb.jpg?type=f_or_off_640_360" className="PcArtistPediaFanPickedLiveItemView_thumbnail__VpXIq" width="351" height="220" alt="">
+                        </img>
                       </div>
                       <em className="PcArtistPediaFanPickedLiveItemView_play_time__Pchic">49:42</em>
                     </div>
@@ -70,6 +84,8 @@ export default function ArtistPedia() {
                   <a className="PcArtistPediaFanPickedLiveItemView_thumbnail_link__exZJV" href="/winner/media/4-195356918">
                     <div className="PcArtistPediaFanPickedLiveItemView_thumbnail_wrap__cKKNC">
                       <div style={{ aspectRatio: 'auto 351 / 220' }}>
+                        <img src="https://phinf.wevpstatic.net/MjAyNTAzMjRfMTUx/MDAxNzQyODIxMzE0ODEz.4SXgSMZgGPtG0HeRyfDHSEcy8l_TNO0SU-KlljU5QGcg.wxKzne3RfYiOU_9lC3THudvQ3n9NXr8X8JslfgNOwX0g.JPEG/thumb.jpg?type=f_or_off_640_360" className="PcArtistPediaFanPickedLiveItemView_thumbnail__VpXIq" width="351" height="220" alt="">
+                        </img>
                       </div>
                       <em className="PcArtistPediaFanPickedLiveItemView_play_time__Pchic">01:50:38</em>
                     </div>
@@ -81,6 +97,8 @@ export default function ArtistPedia() {
                   <a className="PcArtistPediaFanPickedLiveItemView_thumbnail_link__exZJV" href="/winner/media/1-159638774">
                     <div className="PcArtistPediaFanPickedLiveItemView_thumbnail_wrap__cKKNC">
                       <div style={{ aspectRatio: 'auto 351 / 220' }}>
+                        <img src="https://phinf.wevpstatic.net/MjAyNTA1MjdfMjAx/MDAxNzQ4MzQwMDczNDI4.xY4rae1QvB3uTnXXXcfalvheyb3R_bqwpWr661jEqy0g.e8m0nEK7d6qYAs7ZOwwlD8dxsM8sYM-6W_Psgf5FwrQg.JPEG/Weverse_d1799.jpg?type=f_or_off_640_360" className="PcArtistPediaFanPickedLiveItemView_thumbnail__VpXIq" width="351" height="220" alt="">
+                        </img>
                       </div>
                       <em className="PcArtistPediaFanPickedLiveItemView_play_time__Pchic">51:23</em>
                     </div>
@@ -92,6 +110,8 @@ export default function ArtistPedia() {
                   <a className="PcArtistPediaFanPickedLiveItemView_thumbnail_link__exZJV" href="/winner/media/1-156438971">
                     <div className="PcArtistPediaFanPickedLiveItemView_thumbnail_wrap__cKKNC">
                       <div style={{ aspectRatio: 'auto 351 / 220' }}>
+                        <img src="https://phinf.wevpstatic.net/MjAyNTAyMjhfMjI3/MDAxNzQwNzM0MTAzMDk4.xSnBz244zdulJjJso0NTMlwvrUEEdSqHuEgrMDqFFoog.qjrMaet1F6Ayf4fv2ddxv8UCh2G_wlGR7QzTtW3sAAYg.JPEG/image.jpg?type=f_or_off_640_360" className="PcArtistPediaFanPickedLiveItemView_thumbnail__VpXIq" width="351" height="220" alt="">
+                        </img>
                       </div>
                       <em className="PcArtistPediaFanPickedLiveItemView_play_time__Pchic">50:29</em>
                     </div>
@@ -103,6 +123,8 @@ export default function ArtistPedia() {
                   <a className="PcArtistPediaFanPickedLiveItemView_thumbnail_link__exZJV" href="/winner/media/4-202599390">
                     <div className="PcArtistPediaFanPickedLiveItemView_thumbnail_wrap__cKKNC">
                       <div style={{ aspectRatio: 'auto 351 / 220' }}>
+                        <img src="https://phinf.wevpstatic.net/MjAyNTA2MjRfMjUx/MDAxNzUwNzU4OTQ0MTk2.wOmtrJOv5EcNcJZHjz55HIHom_vdd00N8157WhVuHdwg.spRvn8XwFGncDfLbukDa6YeqpEAmPey9KH31uyprusMg.JPEG/Weverse_a0c4c.jpg?type=f_or_off_640_360" className="PcArtistPediaFanPickedLiveItemView_thumbnail__VpXIq" width="351" height="220" alt="">
+                        </img>
                       </div>
                       <em className="PcArtistPediaFanPickedLiveItemView_play_time__Pchic">01:05:18</em>
                     </div>
@@ -114,6 +136,8 @@ export default function ArtistPedia() {
                   <a className="PcArtistPediaFanPickedLiveItemView_thumbnail_link__exZJV" href="/winner/media/1-163042512">
                     <div className="PcArtistPediaFanPickedLiveItemView_thumbnail_wrap__cKKNC">
                       <div style={{ aspectRatio: 'auto 351 / 220' }}>
+                        <img src="https://phinf.wevpstatic.net/MjAyNTA4MThfNDYg/MDAxNzU1NTE0NzU4MzE0.mm0BFile9U3-0ypavs0-f01wNFeO6byq-zNFo6tjZZ8g.G8jo9wNKNaScnrth0_3b7KSUxlOhjsab-_ugEYFw9usg.JPEG/0f70da9b-96e4-4f71-a43e-46a9d77b8066.jpeg?type=f_or_off_640_360" className="PcArtistPediaFanPickedLiveItemView_thumbnail__VpXIq" width="351" height="220" alt="">
+                        </img>
                       </div>
                       <em className="PcArtistPediaFanPickedLiveItemView_play_time__Pchic">01:17:58</em>
                     </div>
@@ -130,6 +154,7 @@ export default function ArtistPedia() {
                   <a href="/winner/media/3-204957412" className="PcArtistPediaMediaOfMonthItemView_container__26enP">
                     <div className="PcArtistPediaMediaOfMonthItemView_preview_area__GIrG7">
                       <div style={{ aspectRatio: 'auto 260 / 146' }}>
+                        <img src="https://phinf.wevpstatic.net/MjAyNTA3MjZfNzIg/MDAxNzUzNDU2NjIyMjEy.uUnZbrg3cacecuNH3eiP18yOQF7YJrvfYTJpWVaVos4g.ReZbZplvlhhsequtfCGhKDaPxKslqXznbx9KGKvCOhAg.JPEG/700cc889-b812-494c-a5c9-5fbd038807b9.jpeg?type=f_or_off_640_360" className="PcArtistPediaMediaOfMonthItemView_preview__UC+OZ" width="260" height="146" alt=""></img>
                       </div>
                     </div>
                     <strong className="PcArtistPediaMediaOfMonthItemView_title__hNMjk">2025 WINNER CONCERT [IN OUR CIRCLE] DAY 1 💙</strong>
@@ -140,6 +165,7 @@ export default function ArtistPedia() {
                   <a href="/winner/media/4-205007818" className="PcArtistPediaMediaOfMonthItemView_container__26enP">
                     <div className="PcArtistPediaMediaOfMonthItemView_preview_area__GIrG7">
                       <div style={{ aspectRatio: 'auto 260 / 146' }}>
+                        <img src="https://phinf.wevpstatic.net/MjAyNTA3MjZfMjkw/MDAxNzUzNTM2NTQyNTI2.iQUJC6mC0IpWix_DHCqdHvxvJ2PHoTX5OsyFnRVCG4Ig.OVP-mZ-TSE5DPSprCwksOHAs54omct3bE-pyjB9IBmog.JPEG/6892a0fc-ea11-4fd0-9e10-33b047215f7c.jpeg?type=f_or_off_640_360" className="PcArtistPediaMediaOfMonthItemView_preview__UC+OZ" width="260" height="146" alt=""></img>
                       </div>
                     </div>
                     <strong className="PcArtistPediaMediaOfMonthItemView_title__hNMjk">2025 WINNER CONCERT [IN OUR CIRCLE] DAY 2 💙</strong>
@@ -150,6 +176,7 @@ export default function ArtistPedia() {
                   <a href="/winner/media/4-205069772" className="PcArtistPediaMediaOfMonthItemView_container__26enP">
                     <div className="PcArtistPediaMediaOfMonthItemView_preview_area__GIrG7">
                       <div style={{ aspectRatio: 'auto 260 / 146' }}>
+                        <img src="https://phinf.wevpstatic.net/MjAyNTA3MjdfMjg4/MDAxNzUzNjIwOTM2MDcw._49FC7CrEHdHE4RsgJdKwKN0ioBn3SYWIRY6qQYG4tEg.8eZoTAL8sLZ8Xrr0TkM4sq-OxQdaXQfsYzy5RE47N7Yg.JPEG/24de23d2-19a5-40d8-88d7-aa7af0a33e10.jpeg?type=f_or_off_640_360" className="PcArtistPediaMediaOfMonthItemView_preview__UC+OZ" width="260" height="146" alt=""></img>
                       </div>
                     </div>
                     <strong className="PcArtistPediaMediaOfMonthItemView_title__hNMjk">2025 WINNER CONCERT [IN OUR CIRCLE] DAY 3 💙</strong>
@@ -160,6 +187,7 @@ export default function ArtistPedia() {
                   <a href="/winner/media/0-162936022" className="PcArtistPediaMediaOfMonthItemView_container__26enP">
                     <div className="PcArtistPediaMediaOfMonthItemView_preview_area__GIrG7">
                       <div style={{ aspectRatio: 'auto 260 / 146' }}>
+                        <img src="https://phinf.wevpstatic.net/MjAyNTA4MTRfNTQg/MDAxNzU1MTYwMzI2MjA0.IkYWpI9v5QHHNWcvVbgGMzcF8fdtwX3LvTPPcFh6cKQg.40HLUnLxJGdlJwAaYBccOHiB1vF9hunO2maOjY86V-kg.JPEG/fbd6e14c-a7fa-47d2-a1c5-70b3b0d8b8a0.jpeg?type=f_or_off_640_360" className="PcArtistPediaMediaOfMonthItemView_preview__UC+OZ" width="260" height="146" alt=""></img>
                       </div>
                     </div>
                     <strong className="PcArtistPediaMediaOfMonthItemView_title__hNMjk">WINNER 11th Anniversary</strong>
@@ -177,7 +205,8 @@ export default function ArtistPedia() {
                 <li className="item">
                   <div role="button" tabindex="0" className="DivAsButtonView_div_as_button__jl7Xf PcWeverseShopArrivalItemView_arrival__brlxG PcWeverseShopArrivalItemView_-home__9Ox4o PcWeverseShopArrivalItemView_-dark__cbtG6">
                     <div className="PcWeverseShopArrivalItemView_image_wrapper__68vpb">
-                      <img className="PcWeverseShopArrivalItemView_image__Kt6Sl" src="https://cdn-contents.weverseshop.io/public/shop/f88cb19a128fb6c28f39fecde222d99c.png" alt="" loading="lazy"></img>
+                      <img className="PcWeverseShopArrivalItemView_image__Kt6Sl" src="https://cdn-contents.weverseshop.io/public/shop/f88cb19a128fb6c28f39fecde222d99c.png" alt="" loading="lazy">
+                      </img>
                     </div>
                     <em className="PcWeverseShopArrivalItemView_product__gnn7H">INNER CIRCLE MEMBERSHIP</em>
                     <div className="PcWeverseShopArrivalItemView_price_wrapper__05mXU">
@@ -188,7 +217,8 @@ export default function ArtistPedia() {
                 <li className="item">
                   <div role="button" tabindex="0" className="DivAsButtonView_div_as_button__jl7Xf PcWeverseShopArrivalItemView_arrival__brlxG PcWeverseShopArrivalItemView_-home__9Ox4o PcWeverseShopArrivalItemView_-dark__cbtG6">
                     <div className="PcWeverseShopArrivalItemView_image_wrapper__68vpb">
-                      <img className="PcWeverseShopArrivalItemView_image__Kt6Sl" src="https://cdn-contents.weverseshop.io/public/shop/834f660785b97cb20901109defa59e2e.png" alt="" loading="lazy"></img>
+                      <img className="PcWeverseShopArrivalItemView_image__Kt6Sl" src="https://cdn-contents.weverseshop.io/public/shop/834f660785b97cb20901109defa59e2e.png" alt="" loading="lazy">
+                      </img>
                     </div>
                     <div className="PcWeverseShopArrivalItemView_label_wrapper__9vTcb">
                       <span className="PcWeverseShopArrivalItemView_label__FlsbE PcWeverseShopArrivalItemView_-only__FAIUl">Only</span>
@@ -203,7 +233,8 @@ export default function ArtistPedia() {
                 <li className="item">
                   <div role="button" tabindex="0" className="DivAsButtonView_div_as_button__jl7Xf PcWeverseShopArrivalItemView_arrival__brlxG PcWeverseShopArrivalItemView_-home__9Ox4o PcWeverseShopArrivalItemView_-dark__cbtG6">
                     <div className="PcWeverseShopArrivalItemView_image_wrapper__68vpb">
-                      <img className="PcWeverseShopArrivalItemView_image__Kt6Sl" src="https://cdn-contents.weverseshop.io/public/shop/3f5f0c37fb88eb2f22a66c709b7ef054.png" alt="" loading="lazy"></img>
+                      <img className="PcWeverseShopArrivalItemView_image__Kt6Sl" src="https://cdn-contents.weverseshop.io/public/shop/3f5f0c37fb88eb2f22a66c709b7ef054.png" alt="" loading="lazy">
+                      </img>
                     </div>
                     <div className="PcWeverseShopArrivalItemView_label_wrapper__9vTcb">
                       <span className="PcWeverseShopArrivalItemView_label__FlsbE PcWeverseShopArrivalItemView_-exclusive__c-T6F">EXCLUSIVE</span>
@@ -217,7 +248,8 @@ export default function ArtistPedia() {
                 <li className="item">
                   <div role="button" tabindex="0" className="DivAsButtonView_div_as_button__jl7Xf PcWeverseShopArrivalItemView_arrival__brlxG PcWeverseShopArrivalItemView_-home__9Ox4o PcWeverseShopArrivalItemView_-dark__cbtG6">
                     <div className="PcWeverseShopArrivalItemView_image_wrapper__68vpb">
-                      <img className="PcWeverseShopArrivalItemView_image__Kt6Sl" src="https://cdn-contents.weverseshop.io/public/shop/d2725f5d8253e9bdbd0ad9e6bba521af.png" alt="" loading="lazy"></img>
+                      <img className="PcWeverseShopArrivalItemView_image__Kt6Sl" src="https://cdn-contents.weverseshop.io/public/shop/d2725f5d8253e9bdbd0ad9e6bba521af.png" alt="" loading="lazy">
+                      </img>
                     </div>
                     <em className="PcWeverseShopArrivalItemView_product__gnn7H">単品[HOLIDAY](DIGIPACK ver.)</em>
                     <div className="PcWeverseShopArrivalItemView_price_wrapper__05mXU">
